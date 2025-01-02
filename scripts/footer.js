1f409514db0a6c47db284f3fc32054ffdb1c470a
remote007 <pmnrai2020@gmail.com>
2025-01-02 13:00:51 +0530

    first

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,16 +2,7 @@ export function loadFooter() {
     fetch('./includes/footer.html')
         .then(response => response.text())
         .then(footerHTML => {
-            document.getElementById('footer-div').innerHTML=`<footer class="footer">
-    <div class="footer-content">
-        <ul>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms of Service</a></li>
-            <li><a href="#">Careers</a></li>
-        </ul>
-        <p>&copy; 2025 Caserolle Co. All rights reserved.</p>
-    </div>
-</footer>`;
+            document.getElementById('footer-div').innerHTML=footerHTML;
         })
         .catch(error => {
             console.error('Error loading footer:', error);
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,17 +3,7 @@ export function loadNavbar() {
       .then(response => response.text())
       .then(navbarHTML => {
         //   document.getElementById('navbar-container').innerHTML=navbarHTML;
-        document.getElementById('navbar-container').innerHTML=`<nav class="navbar"><div class="logo">
-        <a href="/">Casserole Co.</a>
-    </div>
-    <ul class="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="#">Menu</a></li>
-        <li><a href="#">Order Online</a></li>
-        <li><a href="#">Locations</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul></nav>`;
+        document.getElementById('navbar-container').innerHTML=navbarHTML;
       })
       .catch(error => {
           console.error('Error loading navbar:', error);
