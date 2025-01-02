4e9d33b8a41b8c9d2235307e740cd0f1991862f7
remote007 <pmnrai2020@gmail.com>
2025-01-02 12:39:43 +0530

    first

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,7 +2,18 @@ export function loadNavbar() {
   fetch('includes/navbar.html')
       .then(response => response.text())
       .then(navbarHTML => {
-          document.getElementById('navbar-container').innerHTML=navbarHTML;
+        //   document.getElementById('navbar-container').innerHTML=navbarHTML;
+        document.getElementById('navbar-container').innerHTML=`<div class="logo">
+        <a href="/">Casserole Co.</a>
+    </div>
+    <ul class="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Order Online</a></li>
+        <li><a href="#">Locations</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>`;
       })
       .catch(error => {
           console.error('Error loading navbar:', error);
